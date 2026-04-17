cf891b16f345e1e62bd03ea25e1ebed4698bf962
Tomaigle <bartek@amtatara.eu>
2026-04-17 15:19:20 +0200

    haskell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -88,3 +88,5 @@ function mkdirg
 end
 
 zoxide init --cmd cd fish | source
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/bartek/.ghcup/bin $PATH # ghcup-env
